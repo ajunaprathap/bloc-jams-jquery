cc00a2f14eea66a9d1f1a51273362dbcae00c5e9
Ajuna Prathap <ajunaprathap@gmail.com>
2019-10-27 13:48:36 -0400

    Updated song-list.js

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -2,7 +2,6 @@
    album.songs.forEach( (song, index) => {
      song.element = $(`
        <tr>
-        <td>${index + 1}</td>
         <td>
           <button>
             <span class="song-number">${index + 1}</span>
@@ -20,7 +19,7 @@
        $('button#play-pause').attr('playState', player.playState);
      });
 
-     
+
      $('#song-list').append(song.element);
    });
  }
