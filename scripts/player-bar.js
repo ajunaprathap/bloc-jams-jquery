089a4e0e60a015fb46b3a3a811022eeaadfc297c
Ajuna Prathap <ajunaprathap@gmail.com>
2019-10-27 12:01:01 -0400

    Checkpoint 18 jquery:completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,13 +10,28 @@
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
 
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+    }, 1000);
+   }
+
   $('button#previous').on('click', function() {
-   if (player.playState !== 'playing') { return; }
+  if (player.playState !== 'playing') { return; }
   const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
   const previousSongIndex = currentSongIndex -1;
   if (previousSongIndex >= album.songs.length) { return; }
